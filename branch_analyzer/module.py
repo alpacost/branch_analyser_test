61dfddc539f89609ede579fa6d43d538ea2b198e
alpacost <yaroslav.sup2@gmail.com>
2024-08-01 22:47:16 +0300

    greater_version feature add

--- branch_analyzer/module.py
+++ branch_analyzer/module.py
@@ -2,7 +2,7 @@ import aiohttp
 import asyncio
 import json
 import time
-from pkg_resources import parse_version
+from cmp_version import VersionString
 
 
 class BranchAnalyzer:
@@ -20,17 +20,44 @@ class BranchAnalyzer:
             async with session.get(self.__url + branch, ) as response:
                 return await response.json(), response.status == 200
 
-    async def analyse(self):
+    async def analyse(self) -> None:
+        """
+        Main function
+        """
         if not self.__first_branch or not self.__second_branch:
-            return False
+            return
         tasks = self.get_branch_data(self.__first_branch), self.get_branch_data(self.__second_branch)
         first_branch_data, second_branch_data = await asyncio.gather(*tasks)
         if not first_branch_data[1] or not second_branch_data[1]:
-            return False
+            return
         first_branch_data, second_branch_data = first_branch_data[0], second_branch_data[0]
         only_in_first = BranchAnalyzer.only_in_one_branch(first_branch_data['packages'], second_branch_data['packages'])
         only_in_second = BranchAnalyzer.only_in_one_branch(second_branch_data['packages'],
                                                            first_branch_data['packages'])
+        greater_version = BranchAnalyzer.get_greater_version_in_first(first_branch_data['packages'],
+                                                                      second_branch_data['packages'])
+        result = {
+            'only in ' + self.__first_branch: only_in_first,
+            'only in ' + self.__second_branch: only_in_second,
+            'version-release greater in ' + self.__first_branch: greater_version
+        }
+        print(json.dumps(result))
+
+    @staticmethod
+    def get_greater_version_in_first(first_branch_packages: list[dict], second_branch_packages: list[dict]) -> list[dict]:
+        """
+        Compare packages version-release. Return fist branch packages which version-release greater than in second.
+        """
+        result = []
+        packages_map = {(package['name'] + package['arch']).strip(): package for package in first_branch_packages}
+        for package in second_branch_packages:
+            second_branch_package = packages_map.get((package['name'] + package['arch']).strip() )
+            if second_branch_package:
+                version1 = VersionString(package['version'] + '-' + package['release'])
+                version2 = VersionString(second_branch_package['version'] + '-' + second_branch_package['release'])
+                if version1 > version2:
+                    result.append(package)
+        return result
 
     @staticmethod
     def only_in_one_branch(first_branch_packages: list[dict], second_branch_packages: list[dict]) -> list[dict]:
